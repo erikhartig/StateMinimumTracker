3056752d211dc28c4ed868a7e53786fbe6e16f2d
Erik Hartig <wishfulthinkin16@gmail.com>
2018-07-26 19:11:08 -0400

    changed login to use jwt instead of session id, untested

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,26 @@
-var Login = require('../models/login');
+const mysql = require('mysql');
+const express = require('express');
+const request = require('request');
+const sjcl = require('sjcl');
+const bodyParser = require('body-parser');
+
+const validForTime = 1800000 //how long a JWT is valid for in ms
+var app = express();
+//creates a connection object to allow access to the database
+var con = mysql.createConnection({
+   host: "localhost",
+   user: "root",
+   password: "dkbrifkvo759603",
+   database: "statemin"
+});
+
 exports.loginPost = function(req, res) {
   login(req, res);
 }
 
+//Data not currently being validated, however is safe
 exports.login = function(req, res) {
-  let sql = "select * from users WHERE username = ?";
+  let sql = "select * from users WHERE username = ??";
   let inserts = [req.body.username];
   sql = mysql.format(sql, inserts);
   con.query(sql, function(err, result, fields) {
@@ -13,16 +29,25 @@ exports.login = function(req, res) {
     let hashedPassword = sjcl.hash.sha256.hash(req.body.password, saltBits, 1000, 256);
     let digest_sha256 = sjcl.codec.hex.fromBits(hashedPassword);
     if (req.body.username == result[0].username && digest_sha256 == result[0].hashed_password) {
-      if (result[0].refresh_token !== null) {}
-      let sessionId = sjcl.random.randomWords(16);
-      let sessionIdHex = sjcl.codec.hex.fromBits(sessionId);
-      con.query("DELETE FROM sessions where user_id = ?", result[0].id, function(err, result, fields) {
-        if (err) throw err;
-      });
-      con.query("INSERT INTO sessions (id, user_id) VALUES (?, ?)", [sessionIdHex, result[0].id], function(err, result, fields) {
-        if (err) throw err;
-        res.status(200);
-        res.send(String(sessionIdHex));
+      //construct header
+      let header = {"alg":"SHA256", "typ":"JWT"};
+      let encodedHeader = btoa(header);
+
+      //construct payload
+      let d = new Date();
+      let currentTime = d.getTime();
+      let expirationTime = currentTime + validForTime;
+      let payload = {"iat":currentTime, "exp":expirationTime, "username":req.body.username, "permission":"basic"};
+      let encodedPayload = btoa(payload);
+
+      //construct signature
+      let key = fs.readFileSync("./keysHMACSecretKey.txt"); // Either from an RBG or a PBKDF.
+      let hmac = new sjcl.misc.hmac(key, sjcl.hash.sha256);
+      let signature = hmac.encrypt(encodedHeader +"."+encodedPayload);
+
+      let jwt = encodedHeader +"." + encodedPayload +"."+ signature;//unecrypted JWT using HMACSHA256
+      
+      res.send(jwt);
       }); //add fields for refresh token token and refresh time
     } else {
       res.status(400);
